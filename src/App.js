5ca36ff9595febfdb4af42645ce3574eda363137
shehan <hasinthashehan768@gmail.com>
2021-07-05 12:04:53 +0530

    new app text update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          this is aws amplify setup
         </a>
       </header>
     </div>
